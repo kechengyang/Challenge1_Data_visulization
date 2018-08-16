83a31e87ff921d60ebea3c2f9e4e11345202c2a7
kechengyang <1019864896.kx@gmail.com>
2018-08-16 11:44:39 -0400

    添加项目文件。

---
+++ RealChart.Designer.cs
@@ -0,0 +1,177 @@
+﻿using cn.bmob.io;
+
+namespace DemoSharp
+{
+    public class ReadFile : BmobTable
+    {
+
+        public BmobFile record { get; set; }
+
+        public override void readFields(BmobInput input)
+        {
+            base.readFields(input);
+            //读取属性值
+            this.record = input.getFile("record");
+        }
+
+        public override void write(BmobOutput output, bool all)
+        {
+            base.write(output, all);
+            //写到发送端
+            output.Put("record", this.record);
+        }
+    }
+
+    partial class RealChart
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnInit = new System.Windows.Forms.Button();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chart1.Location = new System.Drawing.Point(12, 42);
+            this.chart1.Name = "chart1";
+            series3.BorderColor = System.Drawing.Color.Red;
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.LegendText = "Speed";
+            series3.Name = "Series1";
+            series4.BorderColor = System.Drawing.Color.Blue;
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.LegendText = "Distance";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(884, 422);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(821, 10);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(75, 23);
+            this.btnInit.TabIndex = 3;
+            this.btnInit.Text = "Initiation";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Checked = true;
+            this.rb1.Location = new System.Drawing.Point(418, 13);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(59, 16);
+            this.rb1.TabIndex = 4;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "折线图";
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.Visible = false;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(69, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "KM KM/H";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(747, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Time";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // RealChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 476);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.btnInit);
+            this.Controls.Add(this.chart1);
+            this.Name = "RealChart";
+            this.Text = "Network";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RealChart.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+using cn.bmob.api;
+using cn.bmob.tools;
+using System.Diagnostics;
+using cn.bmob.io;
+using System.Net;
+
+namespace DemoSharp
+{
+    public partial class RealChart : Form
+    {
+        private Queue<double> dataQueue = new Queue<double>(100); //距离
+        private Queue<double> dataQueue1 = new Queue<double>(100);//速度
+        private int curValue = 0;
+
+        private int num = 5;//每次删除增加几个点
+
+        public RealChart()
+        {
+            InitializeComponent();
+        }
+
+        public static string HttpDownload(string url, string path)
+        {
+            // 设置参数
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            //发送请求并获取相应回应数据
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            //直到request.GetResponse()程序才开始向目标网页发送Post请求
+            Stream responseStream = response.GetResponseStream();
+
+            //创建本地文件写入流
+            Stream stream = new FileStream(path, FileMode.Create);
+
+            byte[] bArr = new byte[1024];
+            int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            while (size > 0)
+            {
+                stream.Write(bArr, 0, size);
+                size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            }
+            stream.Close();
+            responseStream.Close();
+            return path;
+        }
+
+        // 跨线程通讯Handler代理
+        private delegate void txtHandeler(object obj);
+
+        /// <summary>
+        /// 初始化事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            BmobWindows Bmob = new BmobWindows();
+            Bmob.initialize("38cd909fe640756176303b980453db55", "9e8a836ccfd013f87619b284a97de02c"); // 初始化Bmob客户端
+            BmobDebug.Register(msg => { Debug.WriteLine(msg); }); // 用于调试输出请求参数
+
+            BmobQuery query = new BmobQuery();
+            //新建查询 查询Activityrecord表中所有的数据 按时间正向排序
+            Bmob.Find<ReadFile>("Activityrecord", query, (resp, exception) =>
+            {
+                // 处理查询网络异常
+                if (exception != null)
+                {
+                    return;
+                }
+
+                //对返回结果进行处理
+                List<ReadFile> list = resp.results;
+                foreach (var game in list)
+                {
+                    BmobFile record = game.record;
+                    if(record!=null)
+                    {
+                        if (File.Exists(record.filename))
+                        {
+                            Console.WriteLine("跳过:" + record.filename);
+                        }
+                        else
+                        {
+                            Console.WriteLine("下载:" + record.filename);
+                            //调用HttpDownload进行非重复文件的下载，此处下载为同步下载 即一个下载完才会执行之后代码
+                            HttpDownload(record.url, "./" + record.filename);
+                        }
+                    }
+               
+                }
+                Console.WriteLine("开始写入");
+                String[] files = Directory.GetFiles("./", "*.txt", SearchOption.AllDirectories);
+                foreach (string f in files)
+                {
+                    if (File.Exists("db.txt"))
+                    {
+                        Console.WriteLine("it is existed");
+                    }
+                    else {
+                        FileStream fs = new FileStream("db.txt", FileMode.CreateNew);
+                        fs.Close();
+                    }
+                    Console.WriteLine("查看" + f);
+                    if (f.Contains("db.txt")) continue;
+                    string a = System.IO.File.ReadAllText(f);
+                    string b = System.IO.File.ReadAllText("db.txt");
+                    System.IO.File.WriteAllText("db.txt", a + "\r\n" + b);
+                    File.Delete(f);
+                }
+                byte[] byData = new byte[100];
+                char[] charData = new char[1000];
+                
+                // 这里是Bmob的回调函数 也就是查询完之后执行下载了所有文件再执行此处的合并和InitChart
+                // 所以需要将这个设置为False 否则会提示操作跨线程错误
+                Control.CheckForIllegalCrossThreadCalls = false;
+                InitChart();
+                Read("db.txt");
+            });
+            
+        }
+        public void Read(string path)
+        {
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            while ((line = sr.ReadLine()) != null)//第一次初始化，读取所有人名
+            {
+                string[] split = line.Split(new char[] { ',' }, 10);
+                if(!comboBox1.Items.Contains(split[0]))
+                    comboBox1.Items.Add(split[0]);
+            }
+            sr.Close();
+        }
+        /// <summary>
+        /// 开始事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this.timer1.Start();
+        } 
+        
+        /// <summary>
+        /// 停止事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+        }
+
+        /// <summary>
+        /// 定时器事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        } 
+        
+        /// <summary>
+        /// 初始化图表
+        /// </summary>
+        private void InitChart() {
+            //定义图表区域
+            this.chart1.ChartAreas.Clear();
+            ChartArea chartArea1 = new ChartArea("C1");
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ChartAreas[0].AxisY.Minimum = 0;
+            this.chart1.ChartAreas[0].AxisY.Maximum =40;
+            this.chart1.ChartAreas[0].AxisX.Interval = 1;
+            this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            //this.chart1.ChartAreas[0].AxisX.IntervalOffset = 1.00D;
+
+            //设置标题
+            this.chart1.Titles.Clear();
+            //this.chart1.Titles.Add("S01");
+            //this.chart1.Titles[0].Text = "XXX显示";
+            //this.chart1.Titles[0].ForeColor = Color.RoyalBlue;
+            //this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            //设置图表显示样式
+            this.chart1.Series[0].Color = Color.Red;
+            this.chart1.Series[1].Color = Color.Blue;
+
+            this.chart1.Series[0].LegendText = "Speed";
+            this.chart1.Series[1].LegendText = "Distance";
+            
+            Legend leg = new Legend(chart1.Name);
+            //leg.Position = this.chart1.Top;
+            this.chart1.Legends.Add(leg);
+
+            if (rb1.Checked)
+            {
+              //  this.chart1.Titles[0].Text =string.Format( "XXX {0} 显示",rb1.Text);
+              //  this.chart1.Series[0].ChartType = SeriesChartType.Line;
+            }
+            this.chart1.Series[0].Points.Clear();
+            this.chart1.Series[1].Points.Clear();
+        }
+        
+        //更新队列中的值
+        private void UpdateQueueValue() {
+            dataQueue.Clear();
+            dataQueue1.Clear();
+            StreamReader sr = new StreamReader("db.txt", Encoding.Default);
+            String line;
+            String strname;
+            double distance, speed;
+            strname = comboBox1.Text;
+            while ((line = sr.ReadLine()) != null)//读取选择对应数据
+            {
+                string[] split = line.Split(new char[] { ',' }, 10);
+                
+                if (strname == split[0])
+                {
+                    speed = Convert.ToDouble(split[1]);
+                    distance = Convert.ToDouble(split[2]);
+                    dataQueue.Enqueue(distance);
+                    dataQueue1.Enqueue(speed);
+                }
+            }
+
+            if (dataQueue.Count > 100) {
+                //先出列
+                for (int i = 0; i < num; i++)
+                {
+                    dataQueue.Dequeue();
+                }
+            }
+            
+        }
+
+        private void checkedListBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateQueueValue();
+            label1.Visible = true;
+            label2.Visible = true;
+            this.chart1.Series[0].Points.Clear();
+            this.chart1.Series[1].Points.Clear();
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                this.chart1.Series[0].Points.AddXY(i, dataQueue.ElementAt(i));
+                this.chart1.Series[1].Points.AddXY(i, dataQueue1.ElementAt(i));
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
